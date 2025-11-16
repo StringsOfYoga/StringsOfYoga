4362d8a24dd8c547462b2ac7b64875dce22dbc81
AyushHere <aayushmalviya90@gmail.com>
2025-11-16 22:56:14 +0530

    basic home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { trigger, transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('900ms ease', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
 })
+
 export class HomeComponent {
 
 }
