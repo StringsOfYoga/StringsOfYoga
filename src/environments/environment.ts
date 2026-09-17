c4fc39bae1334632bed09cee55b0f8d9aa608142
StringsOfYoga <Renoo.prashant@gmail.com>
2026-09-17 15:16:40 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://soy.runasp.net/api',
+  apiUrl: 'https://soy.runasp.net/api',
   /** When true, courses/workshops/blog use JSON; media/collections prefer mock files. */
   useMockData: false
 };
